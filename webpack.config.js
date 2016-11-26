1fa31aec7dc0002d64af9c6bff4c2a2e9b42a032
Per Svensson <Per Svensson>
2016-11-26 10:04:23 +0100

    webpack devserver

--- webpack.config.js
+++ webpack.config.js
@@ -6,36 +6,35 @@ var nodeExternals = require('webpack-node-externals');
 var merge = require('webpack-merge');
 var allFilenamesExceptJavaScript = /\.(?!js(\?|$))([^.]+(\?|$))/;
 
-// Configuration in common to both client-side and server-side bundles
-var sharedConfig = () => ({
-    resolve: { extensions: [ '', '.js', '.jsx', '.ts', '.tsx' ] },
-    output: {
-        filename: '[name].js',
-        publicPath: '/scripts/dist/' // Webpack dev middleware, if enabled, handles requests for this URL prefix
-    },
-    module: {
-        loaders: [
-            { test: /\.tsx?$/, include: /ClientApp/, loader: 'babel-loader' },
-            { test: /\.tsx?$/, include: /ClientApp/, loader: 'ts', query: { silent: true } }
-        ]
-    }
-});
-
-// Configuration for client-side bundle suitable for running in browsers
 var clientBundleOutputDir = './wwwroot/scripts/dist';
-var clientBundleConfig = merge(sharedConfig(), {
+var clientBundleConfig = {
+    devServer: {
+        historyApiFallBack: true,
+        // progress: true,
+        //hot: true,
+        //inline: true,
+        // https: true,
+        port: 8080,
+        contentBase: path.resolve(__dirname,'wwwroot'),
+        proxy: {
+            '/api/**': {
+                changeOrigin: true,
+                target: 'http://witf.apphb.com/api',
+                secure: false
+            }
+        }
+    },
+    resolve: { extensions: [ '', '.js', '.jsx', '.ts', '.tsx' ] },
     entry: { 'main-client': './ClientApp/boot-client.tsx' },
     module: {
         loaders: [
-            //Everything inside the styles folder is packaged inte app.css and cant use hot reload
-            //We typically develop in a standalone scss file first and then move it into the css folder.
+            { test: /\.tsx?$/, include: /ClientApp/, loader: 'babel-loader' },
+            { test: /\.tsx?$/, include: /ClientApp/, loader: 'ts', query: { silent: true } },
             {
                 test: /\.scss$/,
-                //include: [path.resolve(__dirname, 'css')],
                 include: [path.resolve(__dirname, 'css')],
                 loader: ExtractTextPlugin.extract("style","css!sass")
             },
-            //All sass NOT in the css folder we include in the bundle (and get it hot reloadable)
             {
                 test: /\.scss$/,
                 include: [path.resolve(__dirname, 'ClientApp')],
@@ -46,6 +45,8 @@ var clientBundleConfig = merge(sharedConfig(), {
         ]
     },
     output: {
+        filename: '[name].js',
+        publicPath: '/scripts/dist/', // Webpack dev middleware, if enabled, handles requests for this URL prefix
         path: path.join(__dirname, clientBundleOutputDir)
     },
     plugins: [
@@ -61,26 +62,6 @@ var clientBundleConfig = merge(sharedConfig(), {
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
     ])
-});
+};
 
-// Configuration for server-side (prerendering) bundle suitable for running in Node
-var serverBundleConfig = merge(sharedConfig(), {
-    entry: { 'main-server': './ClientApp/boot-server.tsx' },
-    output: {
-        libraryTarget: 'commonjs',
-        path: path.join(__dirname, './ClientApp/dist')
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.scss$/,
-                loader: "ignore"
-            }
-        ]
-    },
-    target: 'node',
-    devtool: 'inline-source-map',
-    externals: [nodeExternals({ whitelist: [allFilenamesExceptJavaScript] })] // Don't bundle .js files from node_modules
-});
-
-module.exports = [clientBundleConfig, serverBundleConfig];
+module.exports = clientBundleConfig;
