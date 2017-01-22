053644ab9856893adf602fb755a2e22fe24d8e95
Marius Ingjer <mariusin79@gmail.com>
2017-01-22 11:13:44 +0100

    refactored etter fp from Per

--- ClientApp/components/ingredients/ingredients.tsx
+++ ClientApp/components/ingredients/ingredients.tsx
@@ -12,19 +12,15 @@ export class Ingredients extends React.Component<IIngredientsProps, any> {
 
     render() {
 
-        return <div>
-
-                {this.props.ingredients.reduce((result,ingredient,i) => {
-
-                    if (!ingredient.quantity) {
-                        result.push(<div className="ingredient" key={i}><span className="name">{ingredient.name}</span></div>)
-                    } else {
-                        result.push(<div className="ingredient" key={i}>{ingredient.quantity} <span>{ingredient.unit}</span> <span className="name">{ingredient.name}</span></div>);
-                    }
-
-                    return result;
-                },[])
-                }
-        </div>;
+        return (
+                <div>
+                    {this.props.ingredients.map((ingredient, i) => (
+                        <div className="ingredient" key={i}>
+                            {ingredient.quantity ? <span>{ingredient.quantity} {ingredient.unit}</span> : null}
+                            <span className="name">{ingredient.name}</span>
+                        </div>
+                    ))}
+                </div>
+            )
     }
 }
